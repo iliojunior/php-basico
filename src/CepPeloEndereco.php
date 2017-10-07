99967246b94cc7da4fce3bff70caaa9df850bfcc
Ilio Adriano de Oliveira Junior <ilioadriano@live.com>
2017-10-07 11:49:45 -0300

    ident pelo cep

--- src/CepPeloEndereco.php
+++ src/CepPeloEndereco.php
@@ -2,30 +2,30 @@
 
 namespace Ilio\BuscadorCep;
 
+use GuzzleHttp\Client;
 use Ilio\BuscadorCep\Exception\ErroAoContactarViaCepException;
 use Ilio\BuscadorCep\Interfaces\IBuscable;
-use GuzzleHttp\Client;
 
 class CepPeloEndereco implements IBuscable
 {
-  protected $cep;
+    protected $cep;
 
-  public function __construct($cep) 
-  {
-    $this->cep = $cep;
-  }
-
-  public function buscar()
-  {
-    $client = new Client();
-    $url = sprintf("https://viacep.com.br/ws/%s/json", $this->cep);
-    $response = $client->request('GET', $url);
-
-    if ($response->getStatusCode() != 200)
+    public function __construct($cep) 
     {
-      throw new ErroAoContactarViaCepException();
+        $this->cep = $cep;
     }
 
-    return json_decode($response->getBody());
-  }
+    public function buscar()
+    {
+        $client = new Client();
+        $url = sprintf("https://viacep.com.br/ws/%s/json", $this->cep);
+        $response = $client->request('GET', $url);
+
+        if ($response->getStatusCode() != 200)
+        {
+          throw new ErroAoContactarViaCepException();
+        }
+
+        return json_decode($response->getBody());
+    }
 }
